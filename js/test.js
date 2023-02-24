fcc799199c9934c132ba74a0feea00692393ac66
Tuhin Ahmed <mdtuhin.dev@gmail.com>
2023-02-24 23:15:44 +0600

    video 7-14

---
+++ js/test.js
@@ -0,0 +1,29 @@
+/*Video No.08*/
+
+// var carName;
+carName = "volvo";
+var carName;
+console.log(carName);
+
+/*Video No.09*/
+
+let a; //declare
+a = 5;
+
+a = 7; //reassign
+
+/* deffernt of let and const*/
+
+const b = 5;
+
+b = 7; // non-reassignable (const)
+
+/* Example of constant array*/
+
+const c = [1, 2, 3, 4];
+
+/*Video No.10 is oparetor*/
+/*Video No.11 is Assignment*/
+/*Video No.10 is Data Type*/
+/*Video No.10 is Function*/
+/*Video No.10 is Object*/
